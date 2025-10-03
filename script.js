5ac1645d5f303421953b8a7316f1db8581a03961
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-10-03 14:37:37 +0300

    Handling click event

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🥳 Correct number';
 
@@ -7,3 +8,12 @@ document.querySelector('.number').textContent = 13;
 document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔️ No number';
+  }
+});
