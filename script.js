45ccd35fe1fe8180e9e60c54d654e53c40f99958
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-10-03 15:01:31 +0300

    Implementing the game logic

--- script.js
+++ script.js
@@ -9,11 +9,37 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
+
+const secretNum = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNum;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔️ No number';
+    document.querySelector('.message').textContent = '⛔️ No number!';
+  } else if (guess === secretNum) {
+    document.querySelector('.message').textContent = '🥳 Correct number!';
+  } else if (guess > secretNum) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNum) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
