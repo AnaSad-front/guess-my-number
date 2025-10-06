34c156b68417b929883a530368c01085a0981d03
AnaSad-front <anastasiasadyreva1@gmail.com>
2025-10-06 12:48:06 +0300

    Manipulating CSS styles

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '⛔️ No number!';
   } else if (guess === secretNum) {
     document.querySelector('.message').textContent = '🥳 Correct number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNum) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
